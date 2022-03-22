e5ddcd961ed34ca99060066a8ac5381a578c3d97
JonathanM99 <johny.carp99@gmail.com>
2022-03-22 00:08:35 -0300

    Agregar ItemCount
    MEDINA JONATHAN

--- src/components/pages/Carrito.jsx
+++ src/components/pages/Carrito.jsx
@@ -1,19 +1,17 @@
 import React from 'react'
-import ItemListContainer from '../../container/ItemListContainer/ItemListContainer'
-
+import ItemCount from '../ItemCount/ItemCount'
 
 const carrito = () => {
+
   return (
     <div>
-        <h3>
-            SU CARRITO:
-        </h3>
-        <p>
-            Verifique que los datos sean correctos.
-        </p>
-        <ItemListContainer
-        greeting=''
-      />
+      <h3>
+        SU CARRITO:
+      </h3>
+      <p>
+        Verifique que los datos sean correctos.
+      </p>
+      <ItemCount initial={0} stock={10}/>
     </div>
   )
 }
